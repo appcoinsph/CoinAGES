43e586fd164f97343dc70bdaa195e0138f17f98c
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-10-14 20:55:03 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -101,7 +101,7 @@ var investigationdone = new Howl({
   src: ['../assets/audio/investigationdone.mp3']
 });
 var soundtrack = new Howl({
-  src="https://bnthosting.online/listen/highlights_radio_/stream",
+  src: "https://bnthosting.online/listen/highlights_radio_/stream",
   loop: true,
   volume: 0.125
 });
