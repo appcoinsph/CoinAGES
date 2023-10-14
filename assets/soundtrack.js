7230cc92713764103852dd24a40d140911afa455
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-10-14 20:41:47 +0800

    Create soundtrack.js

---
+++ assets/soundtrack.js
@@ -0,0 +1,8 @@
+<!---partial--->
+<div id="playlist" class="playlist">
+ <button>+</button>
+  <ul>
+    <li><iframe src="https://echozoneph.online/playlist1.html" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="300px" width="250px" allowfullscreen></iframe>
+</li>
+  </ul>
+</div>
