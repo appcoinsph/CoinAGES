b5a106e41aaafb068dca289b3fa8d73e7b2ff25c
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-10-17 14:52:52 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -472,7 +472,7 @@ function managerobot(){
    document.getElementById('robotbar').style.animation = "barframes 1s infinite"
   document.getElementById('robotbar').style.display = "inline-block"
 
-  setTimeout(managerobot2,1200)
+  setTimeout(managerobot2,1400)
 }
 function managerobot2(){
   customers +=1
