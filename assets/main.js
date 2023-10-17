3e2819c292d7a99fc86b25dff4c79fbb40c75e0d
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-10-17 15:02:10 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -476,7 +476,7 @@ function managerobot(){
 }
 function managerobot2(){
   customers +=1
-  player.money += Math.round(parseFloat(robot.value) * 10);
+  player.money += Math.round(parseFloat(robot.value) * 100);
    document.getElementById('robotbar').style.transition = "none"
   document.getElementById('robotbar').style.width = "0%"
   setTimeout(managerobot,100)
